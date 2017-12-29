cb9e9e26b62b84fbb6a7178a08debbf4bfb14599
shireesha <shireesha@quintype.com>
2017-12-29 21:57:43 +0530

    basic score card design

--- js/main.js
+++ js/main.js
@@ -1,33 +1,129 @@
-class FlagIcon extends HTMLElement {
+class ScoreCard extends HTMLElement {
+
   constructor() {
     super();
-    this._countryCode = null;
+
+    this.shadow = this.createShadowRoot();
+    this._complete = 0;
   }
 
-  static get observedAttributes() { return ["country"]; }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    // name will always be "country" due to observedAttributes
-    this._countryCode = newValue;
-    this._updateRendering();
+  get complete() {
+    return this._complete;
   }
+
+  set complete(val) {
+    this.setAttribute('complete', val);
+  }
+
+  static get observedAttributes() {
+    return [ 'complete' ];
+  }
+
+  attributeChangedCallback(name, oldVal, newVal) {
+    var innerBar = this.shadow.querySelector('.progress-bar-inner');
+
+    switch(name) {
+      case 'complete':
+        this._complete = parseInt(newVal, 10) || 0;
+
+        innerBar.style.width = this.complete + '%';
+        innerBar.innerHTML = this.complete + '%';
+    }
+  }
+
   connectedCallback() {
-    this._updateRendering();
+    var template = `
+      <style>
+        .progress-bar {
+          width: 50%;
+          height: 30px;
+          background-color: #EDF2F4;
+          border-radius: 5px;
+          color: #FFF;
+        }
+        .progress-bar-inner {
+          height: 100%;
+          line-height: 30px;
+          background: #2B2D42;
+          text-align: center;
+          border-radius: 5px;
+          transition: width 0.25s;
+        }
+      </style>
+      <div class="progress-bar">
+        <div class="progress-bar-inner">${this.complete}%</div>
+      </div>
+    `;
+
+    this.shadow.innerHTML = template;
   }
 
-  get country() {
-    return this._countryCode;
-  }
-  set country(v) {
-    this.setAttribute("country", v);
+}
+
+dragElement(document.getElementById("container"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById("header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById("header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    //elmnt.onmousedown = dragMouseDown;
   }
 
-  _updateRendering() {
-    // Left as an exercise for the reader. But, you'll probably want to
-    // check this.ownerDocument.defaultView to see if we've been
-    // inserted into a document with a browsing context, and avoid
-    // doing any work if not.
+  function dragMouseDown(e) {
+    e = e || window.event;
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
   }
 }
 
-customElements.define("flag-icon", FlagIcon);
+p.addEventListener('click', function init() {
+    p.removeEventListener('click', init, false);
+    p.className = p.className + ' resizable';
+    var resizer = document.createElement('div');
+    resizer.className = 'resizer';
+    p.appendChild(resizer);
+    resizer.addEventListener('mousedown', initDrag, false);
+}, false);
+
+var startX, startY, startWidth, startHeight;
+
+function initDrag(e) {
+   startX = e.clientX;
+   startY = e.clientY;
+   startWidth = parseInt(document.defaultView.getComputedStyle(p).width, 10);
+   startHeight = parseInt(document.defaultView.getComputedStyle(p).height, 10);
+   document.documentElement.addEventListener('mousemove', doDrag, false);
+   document.documentElement.addEventListener('mouseup', stopDrag, false);
+}
+
+function doDrag(e) {
+   p.style.width = (startWidth + e.clientX - startX) + 'px';
+   p.style.height = (startHeight + e.clientY - startY) + 'px';
+}
+
+window.customElements.define('score-card', ScoreCard);
