a69e5a41839c93a8865f88da67309baef3b6146c
shireesha <shireesha@quintype.com>
2017-12-31 07:33:07 +0530

    add data

--- js/main.js
+++ js/main.js
@@ -4,126 +4,53 @@ class ScoreCard extends HTMLElement {
     super();
 
     this.shadow = this.createShadowRoot();
-    this._complete = 0;
+    this._stage = 0;
   }
 
-  get complete() {
-    return this._complete;
+  get countryScoreDetails() {
+    return "INDIA 187"+ "/" + "7" + " (20 Overs) " + `<span class="versus">VS</span>` + " " + "SRI LANKA 140" + "(18 overs)";
   }
 
-  set complete(val) {
-    this.setAttribute('complete', val);
+  get matchDetails() {
+    const details = "Sri Lanka in India, 3 T20 International Series, 2017 Time: 13:30 (GMT) | 19:00 (Local time) - 24 Dec 2017";
+    return details;
   }
 
-  static get observedAttributes() {
-    return [ 'complete' ];
+  get MatchImage() {
+    return `<picture>
+          <source media="(min-width: 650px)" srcset="./cricket.jpeg">
+          <source media="(min-width: 465px)" srcset="./cricket.jpeg">
+          <img src="./cricket.jpeg" alt="cricket" style="width:auto;">
+         </picture>`;
   }
 
-  attributeChangedCallback(name, oldVal, newVal) {
-    var innerBar = this.shadow.querySelector('.progress-bar-inner');
 
-    switch(name) {
-      case 'complete':
-        this._complete = parseInt(newVal, 10) || 0;
-
-        innerBar.style.width = this.complete + '%';
-        innerBar.innerHTML = this.complete + '%';
-    }
+  get renderSequence() {
+    console.log(document);
+    let sequence = [this.countryScoreDetails, this.matchDetails, this.MatchImage];
+    return sequence;
   }
+
+
 
   connectedCallback() {
     var template = `
       <style>
-        .progress-bar {
-          width: 50%;
-          height: 30px;
-          background-color: #EDF2F4;
-          border-radius: 5px;
-          color: #FFF;
-        }
-        .progress-bar-inner {
-          height: 100%;
-          line-height: 30px;
-          background: #2B2D42;
-          text-align: center;
-          border-radius: 5px;
-          transition: width 0.25s;
-        }
+      .card-text {
+        margin: 0 auto;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
       </style>
-      <div class="progress-bar">
-        <div class="progress-bar-inner">${this.complete}%</div>
+      <div class="score-container">
+        <h1 class="card-text">Cricket Score Card</h1>
+        <div id="y" class="card-content">${this.renderSequence[0]}</div>
       </div>
     `;
-
     this.shadow.innerHTML = template;
   }
 
-}
-
-dragElement(document.getElementById("container"));
-
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById("header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById("header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    //elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
-
-p.addEventListener('click', function init() {
-    p.removeEventListener('click', init, false);
-    p.className = p.className + ' resizable';
-    var resizer = document.createElement('div');
-    resizer.className = 'resizer';
-    p.appendChild(resizer);
-    resizer.addEventListener('mousedown', initDrag, false);
-}, false);
-
-var startX, startY, startWidth, startHeight;
-
-function initDrag(e) {
-   startX = e.clientX;
-   startY = e.clientY;
-   startWidth = parseInt(document.defaultView.getComputedStyle(p).width, 10);
-   startHeight = parseInt(document.defaultView.getComputedStyle(p).height, 10);
-   document.documentElement.addEventListener('mousemove', doDrag, false);
-   document.documentElement.addEventListener('mouseup', stopDrag, false);
-}
-
-function doDrag(e) {
-   p.style.width = (startWidth + e.clientX - startX) + 'px';
-   p.style.height = (startHeight + e.clientY - startY) + 'px';
 }
 
 window.customElements.define('score-card', ScoreCard);
